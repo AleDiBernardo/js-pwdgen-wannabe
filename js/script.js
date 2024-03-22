7a98fac3a20b6a00aa686d769101ae3fda27ba9c
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-22 14:26:09 +0100

    feat: add some style

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
-// GET DATA AND VARIABLES DECLARATION
+/*
+    ! X LORIS E LUCA:
 
-let userName = prompt("Inserisci il tuo nome");
-let userSurname = prompt("Inserisci il tuo cognome");
-let preferredColor = prompt("Inserisci il tuo colore preferito");
+    TRANQUILLI OGGI NON VI FARÒ GIRARE LA TESTA,
+    PERÒ NON ABITUATEVI TROPPO A QUESTA CALMA...
+
+    BUON WEEKEND E STACCATE UN PO' ANCHE VOI!!
+*/
+
+
+// GET DATA AND VARIABLES DECLARATION
+let userName = prompt("Inserisci il tuo nome"); //string
+let userSurname = prompt("Inserisci il tuo cognome"); //string
+let preferredColor = prompt("Inserisci il tuo colore preferito"); //string 
 
 // LOGICAL BLOCK
-let randNum = Math.floor(Math.random() * 100); 
-let inputResult = userName + userSurname + preferredColor + randNum;
+let randNum = Math.floor(Math.random() * 100); //number
+let inputResult = userName + userSurname + preferredColor + randNum; //string
 
 
 
 // PRINT OUTPUT
 document.getElementById("result").innerHTML = `
 
-    <h1>${inputResult}</h1>
+    <span class="fs-2 text-white fw-bold"> ${inputResult}</span>
 `
